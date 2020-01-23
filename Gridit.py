b5e14c85d8a36d44ff6ac872fe9942af205a30c3
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-01-23 17:13:18 +1300

    F

--- Gridit.py
+++ Gridit.py
@@ -3,33 +3,103 @@ from farmware_tools import device
 from farmware_tools import env
 from farmware_tools import get_config_value
 
-try:
-    device.log(message='Setting variables', message_type='success')
-    rows = get_config_value(farmware_name='Gridit', config_name='rows', value_type=int)
-    cols = get_config_value(farmware_name='Gridit', config_name='cols', value_type=int)
-    spaceBetweenRows = get_config_value(farmware_name='Gridit', config_name='spaceBetweenRows', value_type=float)
-    spaceBetweenCols = get_config_value(farmware_name='Gridit', config_name='spaceBetweenCols', value_type=float)
-    startX = get_config_value(farmware_name='Gridit', config_name='startX', value_type=float)
-    startY = get_config_value(farmware_name='Gridit', config_name='startY', value_type=float)
-    startZ = get_config_value(farmware_name='Gridit', config_name='startZ', value_type=float)
 
-    # *** Start Test Move ***
+# Values for testing
+# rows = 4
+# cols = 7 
+# spaceBetweenRows = 47
+# spaceBetweenColumns = 45 
+# startX = 310.2
+# startY = 563.8
+# startZ = 210.96
+# sequenceBeforeMove = 'PickUpSeed' 
+# sequenceAfterMove = 'PlantSeed'
 
+device.log(message='Setting variables', message_type='success')
+rows = get_config_value(farmware_name='Gridit', config_name='rows', value_type=int)
+cols = get_config_value(farmware_name='Gridit', config_name='cols', value_type=int)
+spaceBetweenRows = get_config_value(farmware_name='Gridit', config_name='spaceBetweenRows', value_type=float)
+spaceBetweenCols = get_config_value(farmware_name='Gridit', config_name='spaceBetweenCols', value_type=float)
+startX = get_config_value(farmware_name='Gridit', config_name='startX', value_type=float)
+startY = get_config_value(farmware_name='Gridit', config_name='startY', value_type=float)
+startZ = get_config_value(farmware_name='Gridit', config_name='startZ', value_type=float)
+sequenceBeforeMove = get_config_value(farmware_name='Gridit', config_name='sequenceBeforeMove', value_type=str)
+sequenceAfterMove = get_config_value(farmware_name='Gridit', config_name='sequenceAfterMove', value_type=str)
+
+
+device.log(message='Setting sequenceId variables', message_type='success')
+if sequenceBeforeMove != "":
+    sequenceBeforeMoveId = app.find_sequence_by_name(name=sequenceBeforeMove)
+else :
+    sequenceBeforeMoveId = 0
+
+if sequenceAfterMove != "":
+    sequenceAfterMoveId = app.find_sequence_by_name(name=sequenceAfterMove)
+else :
+    sequenceAfterMoveId = 0
+
+# *** Start Test Move ***
+
+# Initialise or increment x, z position
+xPos = startX + (spaceBetweenRows * r)
+zPos = startZ
+
+ # Set y position back to the begining of the row
+yPos = startY
+
+# moveAbsolute(xPos, yPos, startZ)
+device.log('Moving to ' + str(xPos) + ', ' + str(yPos) + ', ' + str(zPos), 'success', ['toast'])
+device.move_absolute(
+    {
+        'kind': 'coordinate',
+        'args': {'x': xPos, 'y': yPos, 'z': zPos}
+    },
+    100,
+    {
+        'kind': 'coordinate',
+        'args': {'x': 0, 'y': 0, 'z': 0}
+    }
+)
+# *** End Test Move ***
+
+# Start the grid movement
+device.log(message='Starting row loop', message_type='success')
+
+for r in range(rows):
     # Initialise or increment x, z position
-    xPos = startX + (spaceBetweenRows)
+    xPos = startX + (spaceBetweenRows * r)
     zPos = startZ
 
     # Set y position back to the begining of the row
     yPos = startY
 
-    # moveAbsolute(xPos, yPos, startZ)
-    device.log('Moving to ' + str(xPos) + ', ' + str(yPos) + ', ' + str(zPos), 'success', ['toast'])
+    device.log(message='Set positions', message_type='success')
 
-    device.move_absolute(
-        device.assemble_coordinate(xPos, yPos, zPos),
-        100,
-        device.assemble_coordinate(0, 0, 0))
+    for c in range(cols):
+        # Run the before move sequence
+        if sequenceBeforeMove != "":
+            device.log(message='Execute sequence: ' + sequenceBeforeMove, message_type='success')
+            device.execute(sequenceBeforeMoveId)
 
-    # *** End Test Move ***
-except Exception as error:
-    device.log(repr(error))
+        # moveAbsolute(xPos, yPos, startZ)
+        device.log('Moving to ' + str(xPos) + ', ' + str(yPos) + ', ' + str(zPos), 'success', ['toast'])
+        device.move_absolute(
+            {
+                'kind': 'coordinate',
+                'args': {'x': xPos, 'y': yPos, 'z': zPos}
+            },
+            100,
+            {
+                'kind': 'coordinate',
+                'args': {'x': 0, 'y': 0, 'z': 0}
+            }
+        )
+
+        # Run after move sequence
+        if sequenceAfterMove != "":
+            # runSequence(sequenceAfterMove)
+            device.log(message='Execute sequence: ' + sequenceAfterMove, message_type='success')
+            device.execute(sequenceAfterMoveId)
+
+        # Increment y position
+        yPos = yPos + spaceBetweenCols
